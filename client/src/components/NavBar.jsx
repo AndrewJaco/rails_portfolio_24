98dea1b686286b5ea16fda817cbb2a6900ec6f25
andrewjacobus <andrew@cogini.com>
2024-02-23 17:01:29 +0800

    add routing and runner show page

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function NavBar() {
+  return (
+    <nav>
+      <Link to='/'>Home</Link>
+        |
+      <Link to='/runners'>Runners</Link>
+       | 
+      <Link to='/new'>New Runner</Link>
+    </nav>
+  )
+}
+
+export default NavBar
